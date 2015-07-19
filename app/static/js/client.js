fd56fd82fefcca6b5b672ba5044755537f8517aa
AndrewKaiser <kaisea@rpi.edu>
2015-07-19 00:00:01 -0400

    added minor svg animations

--- app/static/js/client.js
+++ app/static/js/client.js
@@ -28,18 +28,127 @@ $(document).ready(function () {
 		popup.innerHTML = "";
 	});
 
-	adjust();
+	$('.icon').hover(function () {
+		console.log('thios')
+		// this.style.border-radius = '25px'
+	},function () {
+
+	})
+
+	// $('#left').hover(function () {
+	// 	var left = document.getElementById('mural-left');
+	// 	left.style.display = 'block';
+	// 	// $('#mural-left').show();
+	// },
+	// function () {
+	// 	var left = document.getElementById('mural-left');
+	// 	left.style.display = 'none';
+	// });
+	// $('#right').hover(function () {
+	// 	var right = document.getElementById('mural-right');
+	// 	right.style.display = 'block';
+	// 	// $('#mural-left').show();
+	// },
+	$("left").asEventStream('mousemove')
+		.filter(function(v) { 
+			var x = v.clientX;
+			var y = v.clientY;
+			console.log(v)
+			return x > 100 })
+		.onValue(function(v) {
+		console.log("we are over 100: " + v);
+	});
+
+	$('#right').hover( function () {
+		show('mural-right')
+	},
+	function () {
+			hide('mural-right')
+	})
+	$('#left').hover( function () {
+		show('mural-left')
+	},
+	function () {
+			hide('mural-left')
+	})
+	var bacon = $('body').asEventStream('mousemove')
+			.filter(function (v) {
+				console.log(v)
+				return true;
+			})
+	$("body").asEventStream('mousemove')
+     .filter(function(v) { return v > 100; })
+     .onValue(function(v) {
+          console.log("we are over 100: " + v);
+     });
+	var blur = $(window).asEventStream('blur').map(function() {
+  return false;
+});
+var focus =  $(window).asEventStream('focus').map(function() {
+  return true;
+});
+var focused = focus.merge(blur).toProperty(true);
+	// $('#left').on('mouseenter', function () {
+	// 	show('mural-left')
+	// })
+	// $('#right').on('mouseleave', function () {
+	// 	hide('mural-right')
+	// })
+	// $('#left').on('mouseleave', function () {
+	// 	hide('mural-left')
+	// })
+
+
+	// function () {
+	// 	var right = document.getElementById('mural-right');
+	// 	right.style.display = 'none';
+	// });
+	// $('#mural-left').hover(function () {
+	// })
+	loadSvg(function () {
+		adjust();
+	})
 	$(window).resize(function() {
 		adjust();
 	});
 });
-
+function setHover(id) {
+	$('body').click(function () {
+		console.log($(id))
+	})
+	// $(id).hover(function () {
+	// 	console.log('on')
+	// }, function () {
+	// 	console.log('off')
+	// })
+}
+function show(id) {
+	// console.log('show')
+	var selected = document.getElementById(id);
+	var link = '';
+	if (id == 'mural-left') {link = 'left'}
+	if (id == 'mural-right') {link = 'right'}
+	var divlink = document.getElementById(link);
+	selected.style.display = 'block'
+	divlink.style.opacity = 100;
+}
+function hide(id) {
+	// console.log('hide')
+	var selected = document.getElementById(id);
+	var link = '';
+	if (id == 'mural-left') {link = 'left'}
+	if (id == 'mural-right') {link = 'right'}
+	var divlink = document.getElementById(link);
+	divlink.style.opacity = 0;
+	selected.style.display = 'none'
+}
 function adjust() {
 	var mural = $('#proportions');
+	// var height =  0 - mural.height();
+	// mural.css('bottom', height)
 	var windowWidth = window.innerWidth;
 	var muralWidth = mural.width();
 	var side = (windowWidth - muralWidth)/2 + 'px';
 
-	console.log(side)
 	mural.css('left', side);
 }
